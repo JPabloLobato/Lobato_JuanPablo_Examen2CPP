2065ed50438cf14109e716b49286be1c5d7210af
JPabloLobato <jlobagon@myuax.com>
2023-12-14 12:05:15 +0100

    Parte 2 Ej1 1advertencia

--- Estudiantes.cpp
+++ Estudiantes.cpp
@@ -1,3 +1,20 @@
-//
-// Created by Pablo Lobato on 14/12/2023.
-//
+//Parte 1 Ej1
+
+#include <stdio.h>
+
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+void mostrarEstudiante(const struct Estudiante* estudiante) {
+    printf("Nombre: %s\n", estudiante->nombre);
+    printf("Edad: %d\n", estudiante->edad);
+    printf("Promedio: %f\n", estudiante->promedio);
+}
+int main() {
+    struct Estudiante estudiante1 = {"Juan", 20, 8.5};
+    mostrarEstudiante(&estudiante1);
+
+    return 0;
+}
